763f82b8816a2552bd3879fa9a8c905759f4dc52
Nguyen Gia Lac <nggialac99@gmail.com>
2022-11-03 16:17:51 +0700

    nest from scratch

---
+++ src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted with user ID: ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated with user ID: ', this.id);
+  }
+
+  @AfterRemove()
+  logDelete() {
+    console.log('Deleted with user ID: ', this.id);
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() user: UpdateUserDto) {
+    return this.usersService.updateInfo(parseInt(id), user);
+  }
+}
